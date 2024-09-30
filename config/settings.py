ccda972bf23d115db233a7a719aabf12e09f5381
vladimirbutyrskii <vlaver@mail.ru>
2024-09-30 15:00:30 +0300

    settings commit

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "vladbut1955@yandex.ru"
-EMAIL_HOST_PASSWORD = "xzlkdwrgboxuhshf"
+EMAIL_HOST_PASSWORD = "................"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
